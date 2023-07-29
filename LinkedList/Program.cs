86b58b47bd343522abd4d1dd192e763251ccedc6
R.K Nanda <rk@RK-s-MacBook-Air.local>
2023-07-29 12:09:18 +0530

    [RK Nanda] Implementing Simple Linked List

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -4,6 +4,19 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Welcome to Linked List Implementation");
+        LinkedList<int> myList = new LinkedList<int>();
+        myList.AddLast(56);
+        myList.AddLast(30);
+        myList.AddLast(70);
+        //myList.Append(56);
+        //myList.Append(30);
+        //myList.Append(70);
+
+        foreach(var ele in myList)
+        {
+            Console.Write(ele+" ");
+        }
+
         Console.ReadLine();
     }
 }
